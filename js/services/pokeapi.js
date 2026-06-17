854e59d0c0ae5ed3a690c8b7254beea882584433
Juan Ruiz <juanruizdevia@gmail.com>
2026-06-17 10:49:51 -0500

    pokeapi.js has been modified

--- js/services/pokeapi.js
+++ js/services/pokeapi.js
@@ -1,1 +1,15 @@
 
+export const getPokemon = (namePokemon) => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + namePokemon;
+
+    return fetch(url)
+        .then(function (response) {
+            if(!response.ok){
+                throw new Error('Pokémon no encontrado');
+            }
+            return response.json();
+        })
+        .then(function (data){
+            return data
+        })
+}
